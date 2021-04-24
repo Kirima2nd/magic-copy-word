f61a5755e9b8321161ffa1ece79cf9d981e78f54
Kirima the 2nd <81698889+Kirima2nd@users.noreply.github.com>
2021-04-24 15:38:45 +0700

    Add asyncio package

--- app.py
+++ app.py
@@ -11,52 +11,62 @@
 #
 # --------------------------------------------
 
-
-import time
 import mouse
 import keyboard
 import pyperclip
+import asyncio
 
 import scrambler
 
-# Always toggle the key false to prevent
-# unnecessary things happend
-toggleKeys = False
+async def main():
+    toggleKeys = False
+        
+    print('------------------------------')
+    print('      Unscrambler Word        ')
+    print('------------------------------')
+    print('                              ')
+    print('Keys List:                    ')
+    print(' * F4      - Toggle ON/OFF    ')
+    print(' * CTRL+C  - Word Unscrambler ')
+    print(' * F10     - Exit Application ')
+    print('------------------------------')
 
-print('------------------------------')
-print('      Unscrambler Word        ')
-print('------------------------------')
-print('                              ')
-print('Keys List:                    ')
-print(' * F4      - Toggle ON/OFF    ')
-print(' * CTRL+C  - Word Unscrambler ')
-print(' * F10     - Exit Application ')
-print('------------------------------')
+    while True:
+        if keyboard.is_pressed('f4'):
+            await asyncio.sleep(1.0)
 
-while True:
-    if keyboard.is_pressed('f4'):
-        if toggleKeys == False:
-            toggleKeys = True
-            print('Key Toggled: ON')
+            if toggleKeys == False:
+                toggleKeys = True
+                print('Key Toggled: ON')
 
-        elif toggleKeys == True:
-            toggleKeys = False
-            print('Key Toggled: OFF')
-        pass
-    elif keyboard.is_pressed('ctrl+c') and toggleKeys:
-        # Why sleep? because without this, pyperclip
-        # cannot grab the copied text on windows
-        # thats why i'm delaying the script to
-        # pyperclip being able to get your copied text
-        time.sleep(1.5)
+            elif toggleKeys == True:
+                toggleKeys = False
+                print('Key Toggled: OFF')
+            pass
 
-        copy_word = pyperclip.paste()
-        result = scrambler.Unscramble(copy_word)
-        tmp = ''.join(result)
-        pyperclip.copy(tmp)
+        elif keyboard.is_pressed('ctrl+c') and toggleKeys:
+            await asyncio.sleep(1.0)
 
-        print(f'Word unscrambled, the result is: {tmp}')
-        pass
-    if keyboard.is_pressed('f10'):
-        print('Exiting Application!')
-        break
+            copy_word = pyperclip.paste()
+            result = await scrambler.Unscramble(copy_word)
+
+            if result == 'Nothing was found.': 
+                another_result = await scrambler.Guess(copy_word)
+
+            print(another_result)
+
+            if result != 'Nothing was found.':
+                tmp = ''.join(result)
+            else:
+                tmp = another_result
+            
+            pyperclip.copy(tmp)
+
+            print(f'Word unscrambled, the result is: {tmp}')
+            pass
+
+        if keyboard.is_pressed('f10'):
+            print('Exiting Application!')
+            break
+
+asyncio.run(main())
